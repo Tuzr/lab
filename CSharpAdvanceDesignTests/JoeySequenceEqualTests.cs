e559a44b07a4efc8904932d4104289bc9ed59324
tuzr <tuzr2002@gmail.com>
2019-03-16 11:44:31 +0800

    seuqence equal first try

--- CSharpAdvanceDesignTests/JoeySequenceEqualTests.cs
+++ CSharpAdvanceDesignTests/JoeySequenceEqualTests.cs
@@ -11,10 +11,44 @@ namespace CSharpAdvanceDesignTests
         public void compare_two_numbers_equal()
         {
             var first = new List<int> { 3, 2, 1 };
-            var second = new List<int> { 3, 2, 1};
-
+            var second = new List<int> { 3, 2, 1 };
             var actual = JoeySequenceEqual(first, second);
+            Assert.IsTrue(actual);
+        }
 
+        [Test]
+        public void compare_two_numbers_equal_second()
+        {
+            var first = new List<int> { 3, 2, 1 };
+            var second = new List<int> { 1, 2, 3 };
+            var actual = JoeySequenceEqual(first, second);
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void compare_two_numbers_equal_third()
+        {
+            var first = new List<int> { 3, 2 };
+            var second = new List<int> { 3, 2, 1 };
+            var actual = JoeySequenceEqual(first, second);
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void compare_two_numbers_equal_fourth()
+        {
+            var first = new List<int> { 3, 2, 1 };
+            var second = new List<int> { 3, 2 };
+            var actual = JoeySequenceEqual(first, second);
+            Assert.IsFalse(actual);
+        }
+
+        [Test]
+        public void compare_two_numbers_equal_fiveth()
+        {
+            var first = new List<int> { };
+            var second = new List<int> { };
+            var actual = JoeySequenceEqual(first, second);
             Assert.IsTrue(actual);
         }
 
