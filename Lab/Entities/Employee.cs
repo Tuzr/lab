5e70b30361a9ead4aa111f55d4af521cdcfa3309
tuzr <tuzr2002@gmail.com>
2019-03-16 14:01:37 +0800

    hash code first version

--- Lab/Entities/Employee.cs
+++ Lab/Entities/Employee.cs
@@ -1,9 +1,44 @@
-﻿namespace Lab.Entities
+﻿using System.Collections.Generic;
+
+namespace Lab.Entities
 {
+    public class JoeyEmployeeWithPhoneEqualityConverter : IEqualityComparer<Employee>
+    {
+        public bool Equals(Employee x, Employee y)
+        {
+            return x.FirstName == y.FirstName
+                   && x.LastName == y.LastName
+                   && x.Phone == y.Phone;
+        }
+
+        public int GetHashCode(Employee obj)
+        {
+            var firstNameHashCode = obj.FirstName.GetHashCode();
+            var lastNameHashCode = obj.LastName.GetHashCode();
+
+            return new {firstNameHashCode, lastNameHashCode}.GetHashCode();
+        }
+    }
+
+    public class JoeyEmployeeEqualityConverter : IEqualityComparer<Employee>
+    {
+        public bool Equals(Employee x, Employee y)
+        {
+            return x.FirstName == y.FirstName 
+                   && x.LastName == y.LastName;
+        }
+
+        public int GetHashCode(Employee obj)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
     public class Employee
     {
         public string LastName { get; set; }
         public string FirstName { get; set; }
         public Role Role { get; set; }
+        public string Phone { get; set; }
     }
 }
