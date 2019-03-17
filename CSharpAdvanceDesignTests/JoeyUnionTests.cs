50a8c2526d976d179441afe9ba1368aaf5e60582
tuzr <tuzr2002@gmail.com>
2019-03-17 10:19:17 +0800

    complete union first

--- CSharpAdvanceDesignTests/JoeyUnionTests.cs
+++ CSharpAdvanceDesignTests/JoeyUnionTests.cs
@@ -1,18 +1,18 @@
 ﻿using ExpectedObjects;
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyUnionTests
     {
         [Test]
         public void union_numbers()
         {
-            var first = new[] { 1, 3, 5 };
-            var second = new[] { 5, 3, 7 };
+            var first = new[] { 1, 3, 5, 3, 1 };
+            var second = new[] { 5, 3, 7, 7 };
 
             var actual = JoeyUnion(first, second);
             var expected = new[] { 1, 3, 5, 7 };
@@ -22,7 +22,28 @@ namespace CSharpAdvanceDesignTests
 
         private IEnumerable<int> JoeyUnion(IEnumerable<int> first, IEnumerable<int> second)
         {
-            throw new System.NotImplementedException();
+            var firstEnumerator = first.GetEnumerator();
+            var seconEnumerator = second.GetEnumerator();
+
+            var hashSet = new HashSet<int>();
+
+            while (firstEnumerator.MoveNext())
+            {
+                var item = firstEnumerator.Current;
+                if (hashSet.Add(item))
+                {
+                    yield return item;
+                }
+            }
+
+            while (seconEnumerator.MoveNext())
+            {
+                var item = seconEnumerator.Current;
+                if (hashSet.Add(item))
+                {
+                    yield return item;
+                }
+            }
         }
     }
 }
