bd53c90d67b5f50a6e7940c406a0adb46c67c1b5
tuzr <tuzr2002@gmail.com>
2019-03-17 11:09:19 +0800

    Except complete

--- CSharpAdvanceDesignTests/JoeyExceptTests.cs
+++ CSharpAdvanceDesignTests/JoeyExceptTests.cs
@@ -1,18 +1,18 @@
 ﻿using ExpectedObjects;
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyExceptTests
     {
         [Test]
         public void except_numbers()
         {
-            var first = new[] { 1, 3, 5, 7 };
-            var second = new[] { 7, 1, 4 };
+            var first = new[] { 1, 3, 5, 7, 3 };
+            var second = new[] { 7, 1, 4, 1 };
 
             var actual = JoeyExcept(first, second);
             var expected = new[] { 3, 5 };
@@ -22,7 +22,18 @@ namespace CSharpAdvanceDesignTests
 
         private IEnumerable<int> JoeyExcept(IEnumerable<int> first, IEnumerable<int> second)
         {
-            throw new System.NotImplementedException();
+            var firstEnumerator = first.GetEnumerator();
+
+            var hashSet = new HashSet<int>(second);
+
+            while (firstEnumerator.MoveNext())
+            {
+                var item = firstEnumerator.Current;
+                if (hashSet.Add(item))
+                {
+                    yield return item;
+                }
+            }
         }
     }
 }
