ce7c49682b407ae2249e41f902fe882bb3f47a35
tuzr <tuzr2002@gmail.com>
2019-03-17 09:47:13 +0800

    增加自訂義comparer

--- CSharpAdvanceDesignTests/JoeyDistinctTests.cs
+++ CSharpAdvanceDesignTests/JoeyDistinctTests.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 using System.Collections.Generic;
+using Lab.Entities;
 
 namespace CSharpAdvanceDesignTests
 {
@@ -12,14 +13,55 @@ namespace CSharpAdvanceDesignTests
         public void distinct_numbers()
         {
             var numbers = new[] { 91, 3, 91, -1 };
-            var actual = Distinct(numbers);
+            var actual = JoeyDistinct(numbers);
 
             var expected = new[] { 91, 3, -1 };
 
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
-        private IEnumerable<int> Distinct(IEnumerable<int> numbers)
+
+        [Test]
+        public void distinct_employees()
+        {
+            var employees = new[]
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Joseph", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+            };
+
+            var actual = JoeyDistinctWithEqualityComparer(employees, new JoeyEmployeeEqualityConverter());
+
+            var expected = new[]
+            {
+                new Employee {FirstName = "Joey", LastName = "Chen"},
+                new Employee {FirstName = "Joseph", LastName = "Chen"},
+                new Employee {FirstName = "Tom", LastName = "Li"},
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        private IEnumerable<Employee> JoeyDistinctWithEqualityComparer<Employee>(IEnumerable<Employee> employees, IEqualityComparer<Employee> comparer)
+        {
+            var enumerator = employees.GetEnumerator();
+
+            var  hashSet = new HashSet<Employee>(comparer);
+
+            while (enumerator.MoveNext())
+            {
+                var item = enumerator.Current;
+                if (hashSet.Add(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+
+        private IEnumerable<int> JoeyDistinct(IEnumerable<int> numbers)
         {
             var numbersEnumerator = numbers.GetEnumerator();
 
@@ -30,9 +72,9 @@ namespace CSharpAdvanceDesignTests
                 var item = numbersEnumerator.Current;
 
                 if (hashSet.Add(item))
-                {                    
+                {
                     yield return item;
-                }                
+                }
             }
         }
     }
